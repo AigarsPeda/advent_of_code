d1015ade1b7733d3e0950fd5a86be557424ba1e3
A.Peda <a.peda@lvm.lv>
2025-02-20 11:26:41 +0200

    Add JSON data files and implement bbox calculation for features

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,50 @@
 import path from "path";
-import getDayFourPartOne from "./days/dayFour/dayFourFirstPart";
+// import getDayFourPartOne from "./days/dayFour/dayFourFirstPart";
 import readFile from "./utils/readFile";
+import bbox from "@turf/bbox";
+import { writeFile } from "fs/promises";
 
-const filePath = path.join(__dirname, "./days/dayFour/dayFour.txt");
+const filePath = path.join(
+  __dirname,
+  "./days/planosanas_vienibas/planosanas_vienibas.json"
+);
+const outputFilePath = path.join(
+  __dirname,
+  "./days/planosanas_vienibas/planosanas_vienibas_formatted.json"
+);
 
 console.log("Hello world!!!!");
 
 const program = async () => {
   const data = await readFile(filePath);
-  const partOne = getDayFourPartOne(data || "");
+  const jsonData = JSON.parse(data || "");
 
-  console.log("partOne", partOne);
+  const newData = jsonData.features.map((feature: any) => {
+    const { geometry, ...rest } = feature;
+    const [minX, minY, maxX, maxY] = bbox({
+      type: "Polygon",
+      coordinates: geometry.rings,
+    });
+    return {
+      ...rest,
+      bbox: [minX, minY, maxX, maxY],
+    };
+  });
+  await writeFile(
+    outputFilePath,
+    JSON.stringify({ features: newData }, null, 2)
+  );
+  console.log("newData", newData);
 };
 
 program();
+
+// attributes: {
+//   OBJECTID: 9619,
+//   LVM_DISTRICT_CODE: '22222',
+//   LVM_DISTRICT_NAME: 'Rendas',
+//   LVM_FORESTRY_CODE: '2',
+//   DISTRICTAREA: null,
+//   MANAGER: '1967'
+// },
+// geometry: { rings: [Array] }
